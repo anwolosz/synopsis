0a7d96423c9e69e58c4c75866d15352bc7ffe33d
janowol <jjjkamu@gmail.com>
2024-08-21 16:43:25 +0200

    Add whitespace

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -2,5 +2,5 @@
 set -ex
 SCRIPT_FOLDER=$(dirname "${0}") 
 WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../../")
-JSON_FOLDER="$WORKSPACE_FOLDER/src/assets/"
+JSON_FOLDER="$WORKSPACE_FOLDER/src/assets/" 
 python3 "$SCRIPT_FOLDER"/json_test.py "$JSON_FOLDER"
